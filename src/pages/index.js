816e162e5797a5560fdcd6b2bf0e830239631caf
Andy Feetenby <andyfeetenby@gmail.com>
2024-04-01 18:42:40 +0700

    set up fetch request to register url

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,6 @@ export { HomePage } from './Home/HomePage';
 export { Hero } from './Home/components/Hero';
 export { ProductsList } from './Products/ProductsList';
 export { ProductDetail } from './ProductDetail';
+
+export { Login } from './Login';
+export { Register } from './Register';

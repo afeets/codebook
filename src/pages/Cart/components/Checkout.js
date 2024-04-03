297e96c59b572e2c0cc19264f2d557c6607d5559
Andy Feetenby <AndyFeetenby@gmail.com>
2024-04-03 14:34:10 +0700

    Fetch request to place order

--- src/pages/Cart/components/Checkout.js
+++ src/pages/Cart/components/Checkout.js
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useCart } from "../../../context";
 import { json } from "react-router-dom";
 
 export const Checkout = ({ setCheckout }) => {
-  const { total } = useCart();
+  const { cartList, total, clearCart } = useCart();
   const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  
+  const token = JSON.parse(sessionStorage.getItem("token"));
+  const cbid = JSON.parse(sessionStorage.getItem("cbid"));
+
 
   useEffect(() => {
-    const token = JSON.parse(sessionStorage.getItem("token"));
-    const cbid = JSON.parse(sessionStorage.getItem("cbid"));
 
     async function getUser() {
       const response = await fetch(`http://localhost:8000/600/users/${cbid}`, {
@@ -22,7 +26,34 @@ export const Checkout = ({ setCheckout }) => {
       setUser(data);
     }
     getUser();
-  }, []);
+  }, [token, cbid]);
+
+
+  async function handleOrderSubmit(event){
+    event.preventDefault();
+
+    const order = {
+      cartList: cartList,
+      amount_paid: total,
+      quantity: cartList.length,
+      user: {
+        name: user.name,
+        email: user.email,
+        id: user.id
+      }
+    }
+
+    const response = await fetch("http://localhost:8000/660/orders", {
+      method: "POST",
+      headers: {"Content-Type": "application/json", Authorization: `Bearer ${token}`},
+      body: JSON.stringify(order)
+    });
+    
+    const data = await response.json();
+    clearCart();
+    navigate("/");
+
+  }
 
   return (
     <section>
@@ -61,7 +92,7 @@ export const Checkout = ({ setCheckout }) => {
               <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
                 <i className="bi bi-credit-card mr-2"></i>CARD PAYMENT
               </h3>
-              <form className="space-y-6">
+              <form onSubmit={ handleOrderSubmit } className="space-y-6">
                 <div>
                   <label
                     htmlFor="name"

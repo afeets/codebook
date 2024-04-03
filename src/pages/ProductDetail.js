69c089486bbd52a911e39f075d3946ed6d2a13b3
Andy Feetenby <AndyFeetenby@gmail.com>
2024-04-03 11:28:02 +0700

    Added classes to Add To Cart button if product out of stock. Also disabling

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -71,11 +71,22 @@ export const ProductDetail = () => {
             {/* check state of cart */}
             <p className="my-3">
               { 
-                !inCart && <button onClick = {() => addToCart(product) } className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800`}>Add To Cart <i className="ml-1 bi bi-plus-lg"></i></button>
+                !inCart && 
+                <button 
+                  onClick = {() => addToCart(product) } 
+                  className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ${product.in_stock ? "" : "cursor-not-allowed"}`}
+                  disabled={ product.in_stock ? "" : "disabled"}>Add To Cart 
+                  <i className="ml-1 bi bi-plus-lg"></i>
+                </button>
               }
               
               {
-                inCart && <button onClick = {() => removeFromCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800`}  disabled={ product.in_stock ? "" : "disabled" }>Remove Item <i className="ml-1 bi bi-trash3"></i></button>
+                inCart && 
+                <button onClick = {() => removeFromCart(product)} 
+                  className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800`}  
+                  disabled={ product.in_stock ? "" : "disabled" }>Remove Item 
+                  <i className="ml-1 bi bi-trash3"></i>
+                </button>
               }
             </p>
 
